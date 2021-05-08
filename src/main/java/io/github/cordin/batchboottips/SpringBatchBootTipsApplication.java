ab7c942a90aadc8efdb617b02d360d913864b828
Cèsar Ordiñana <cordin@gmail.com>
2021-05-08 14:46:56 +0200

    Add start.spring.io batch project

---
+++ src/main/java/io/github/cordin/batchboottips/SpringBatchBootTipsApplication.java
@@ -0,0 +1,13 @@
+package io.github.cordin.batchboottips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchBootTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchBootTipsApplication.class, args);
+	}
+
+}
